2c65c419723bbb83c32d12ce9f5be5af291ece0b
liamsettles8-hash <liamsettles8@gmail.com>
2026-09-01 23:49:30 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,7 @@
 importScripts("/flamelearning/controller-public/controller.sw.js");
 
 self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
 
-self.addEventListener("fetch", event => {
-  try {
-    if ($scramjetController.shouldRoute(event)) {
-      event.respondWith($scramjetController.route(event));
-    }
-  } catch (error) {
-    console.error("[FlameBrowser SW] routing error", error);
-  }
+self.addEventListener("activate", event => {
+    event.waitUntil(self.clients.claim());
 });
