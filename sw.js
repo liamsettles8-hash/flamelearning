0eb8ea34faa8b8be0880b2bc24b5f687e5851ae9
liamsettles8-hash <liamsettles8@gmail.com>
2026-09-01 23:58:17 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-importScripts("/flamelearning/controller-public/controller.sw.js");
-
-self.addEventListener("install", () => {
-    self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-    event.waitUntil(self.clients.claim());
-});
