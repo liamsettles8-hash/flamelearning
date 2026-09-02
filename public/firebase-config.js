7a477efc1ff5536605313e9d6346f090af1d1ce4
liamsettles8-hash <liamsettles8@gmail.com>
2026-09-01 22:50:13 -0500

    Add files via upload

---
+++ public/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDmO8ylA2-bdEAt1HppSk8lMAn2UTVBM4E",
+  authDomain: "flameproxy-30d7f.firebaseapp.com",
+  projectId: "flameproxy-30d7f",
+  storageBucket: "flameproxy-30d7f.firebasestorage.app",
+  messagingSenderId: "970637282698",
+  appId: "1:970637282698:web:5bff179aa77cc184ce4c73",
+  measurementId: "G-QY0JMLW14C"
+};
